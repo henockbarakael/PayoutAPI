c8d47f59f2ce1a3ab49e6b69c5f894a9c0a9c037
unknown <barahenock@gmail.com>
2022-12-22 13:40:03 +0100

    perform footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -6,7 +6,7 @@
             </div>
             <div class="col-sm-6">
                 <div class="text-sm-end d-none d-sm-block">
-                    Design & Develop by Freshpay Team
+                    Design & Develop by  BARAKAEL H.
                 </div>
             </div>
         </div>
