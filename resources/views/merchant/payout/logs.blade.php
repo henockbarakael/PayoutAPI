ba84d6f70e8835629009f824a12725c3714506ba
Henock BARAKAEL <barahenock@gmail.com>
2023-03-02 16:10:01 +0100

    datatable

--- resources/views/merchant/payout/logs.blade.php
+++ resources/views/merchant/payout/logs.blade.php
@@ -62,6 +62,28 @@
     </div>
 </div>
     @section('script')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.3.0/jquery.form.min.js"></script>
+    <script src="{{ asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/simplebar/simplebar.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/node-waves/waves.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/feather-icons/feather.min.js') }}"></script>
+    <script src="{{ asset('assets/js/pages/plugins/lord-icon-2.1.0.js') }}"></script>
+    {{-- <script src="{{ asset('assets/js/plugins.js') }}"></script> --}}
+    <script src="{{ asset('assets/js/app.js') }}"></script>
+    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.2.2/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.print.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.html5.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="{{ asset('assets/libs/list.js/list.min.js')}}"></script>
+    <script src="{{ asset('assets/libs/list.pagination.js/list.pagination.min.js')}}"></script>
+    <script src="{{ asset('assets/libs/sweetalert2/sweetalert2.min.js')}}"></script>
+    <script src="{{ asset('assets/js/pages/datatables.init.js')}}"></script>
     <script type="text/javascript">
         $(document).ready(function () {
             var today = new Date();
