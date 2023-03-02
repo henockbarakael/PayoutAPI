efc4275da2bbd7c3e8c74044c1776a9d28f7ca51
Henock BARAKAEL <barahenock@gmail.com>
2023-03-02 06:22:50 +0100

    update callback response 2

--- app/Http/Controllers/api/CallbackController.php
+++ app/Http/Controllers/api/CallbackController.php
@@ -11,24 +11,24 @@ class CallbackController extends Controller
 {
     public function getCallbackResponse(Request $request){
 
-        $data = $request->getContent();
+        $data = $request->all();
 
-        $dataToSend =  [
-            "status" => $request->Status,
-            "comment" => $request->Comment,
-            "trans_status" => $request->Trans_Status,
-            "currency" => $request->Currency,
-            "amount" => $request->Amount,
-            "method" => $request->Method,
-            "customer_details" => $request->Customer_Details,
-            "reference" => $request->Reference,
-            "paydrc_reference" => $request->PayDRC_Reference,
-            "action" => $request->Action,
-            "status_description" => $request->Status_Description,
-            "trans_status_description" => $request->Trans_Status_Description
-        ];
+        // $dataToSend =  [
+        //     "status" => $request->Status,
+        //     "comment" => $request->Comment,
+        //     "trans_status" => $request->Trans_Status,
+        //     "currency" => $request->Currency,
+        //     "amount" => $request->Amount,
+        //     "method" => $request->Method,
+        //     "customer_details" => $request->Customer_Details,
+        //     "reference" => $request->Reference,
+        //     "paydrc_reference" => $request->PayDRC_Reference,
+        //     "action" => $request->Action,
+        //     "status_description" => $request->Status_Description,
+        //     "trans_status_description" => $request->Trans_Status_Description
+        // ];
 
-        CallbackResponse::create($dataToSend);
+        // CallbackResponse::create($dataToSend);
         Callback::create(["data" =>$data]);
     }
 }
