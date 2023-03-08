3836d8caa923c982028427d0a16901e7e659eb65
Henock BARAKAEL <barahenock@gmail.com>
2023-03-08 17:58:46 +0100

    checking session

--- config/session.php
+++ config/session.php
@@ -155,7 +155,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', 'bulkpayment.gofreshpay.com'),
+    'domain' => env('SESSION_DOMAIN', null),
 
     /*
     |--------------------------------------------------------------------------
