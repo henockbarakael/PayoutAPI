856b8de527c27d7c5720a85bb56fe76d5d7560ba
Henock BARAKAEL <barahenock@gmail.com>
2023-03-10 04:55:31 +0100

    MerchantController Datatable

--- resources/views/admin/user/all.blade.php
+++ resources/views/admin/user/all.blade.php
@@ -11,11 +11,6 @@
         @include('layouts.page-title')
         <!-- end page title -->
 
-        <div class="alert alert-danger" role="alert">
-            This is <strong>Datatable</strong> page in wihch we have used <b>jQuery</b> with cnd link!
-        </div>
-
-
         <div class="row">
             <div class="col-lg-12">
                 <div class="card">
@@ -23,7 +18,7 @@
                         <h5 class="card-title mb-0">Liste des utilisateurs</h5>
                     </div>
                     <div class="card-body">
-                        <table id="buttons-datatables" class="display table table-bordered" style="width:100%">
+                        <table class="table nowrap align-middle" id="merchant_list" style="width:100%">
                             <thead>
                                 <tr>
                                     <th>#</th>
@@ -68,4 +63,57 @@
     </div>
     <!-- container-fluid -->
 </div>
+@section('script')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.3.0/jquery.form.min.js"></script>
+<script src="{{ asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('assets/libs/simplebar/simplebar.min.js') }}"></script>
+<script src="{{ asset('assets/libs/node-waves/waves.min.js') }}"></script>
+<script src="{{ asset('assets/libs/feather-icons/feather.min.js') }}"></script>
+<script src="{{ asset('assets/js/pages/plugins/lord-icon-2.1.0.js') }}"></script>
+{{-- <script src="{{ asset('assets/js/plugins.js') }}"></script> --}}
+<script src="{{ asset('assets/js/app.js') }}"></script>
+<script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.2/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.print.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.html5.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="{{ asset('assets/libs/list.js/list.min.js')}}"></script>
+<script src="{{ asset('assets/libs/list.pagination.js/list.pagination.min.js')}}"></script>
+<script src="{{ asset('assets/libs/sweetalert2/sweetalert2.min.js')}}"></script>
+<script src="{{ asset('assets/js/pages/datatables.init.js')}}"></script>
+<script type="text/javascript">
+    $(document).ready(function () {
+        var today = new Date();
+        var n = today.getDate().toString()+(today.getMonth()+1)+today.getFullYear();
+        $('#merchant_list').DataTable({
+            scrollX: true,
+            dom:"Bfrtip",
+            order: [[0, 'desc']],
+            buttons: [
+                    {
+                        extend: 'csv',
+                        filename: 'Merchant_List.' + n
+                    },
+                    {
+                        extend: 'excel',
+                        filename: 'Merchant_List.' + n
+                    },
+                    {
+                        extend: 'pdf',
+                        filename: 'Merchant_List.' + n
+                    }
+                ],
+            lengthMenu: [
+                [10, 25, 50, -1],
+                [10, 25, 50, 'All'],
+            ],
+        });
+    });
+</script>
+@endsection
 @endsection
